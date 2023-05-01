1351e5ea3b4c64b48ed551810d2b9fa8ecbd9614
Sajal Shrestha <sajal@Sajals-MacBook-Pro.local>
2023-05-01 15:13:20 -0700

    final push

--- pseudo-otp.py
+++ pseudo-otp.py
@@ -1,76 +1,79 @@
 import argparse
-import secrets
+import random
 
 
-def gen_key(n: int) -> str:
-    """
-    Generate an n-bit pseudorandom key using the secrets module.
-    """
-    return bin(secrets.randbits(n))[2:].zfill(n)
+def gen(n):
+    """Generate an n-bit random key"""
+    return bin(random.getrandbits(n))[2:].zfill(n)
 
 
-def encrypt(plaintext: str, key: str) -> str:
-    """
-    Encrypt plaintext using the one-time pad scheme with a PRG key.
-    """
-    if len(key) < len(plaintext):
-        raise ValueError("Key must be at least as long as the plaintext")
-    # Convert plaintext and key to binary strings
-    plaintext_bits = ''.join(format(ord(c), '08b') for c in plaintext)
-    key_bits = ''.join(format(int(c), '08b') for c in key)
-    # Generate a pseudorandom bit string the same length as the plaintext
-    prg_bits = bin(secrets.randbits(len(plaintext_bits)))[
-        2:].zfill(len(plaintext_bits))
-    # XOR the plaintext and PRG bits using Python's built-in bitwise XOR operator (^)
-    ciphertext_bits = ''.join(str(int(a) ^ int(b))
-                              for a, b in zip(plaintext_bits, prg_bits))
-    # Convert the ciphertext binary string back to ASCII
-    ciphertext = ''.join(
-        chr(int(ciphertext_bits[i:i+8], 2)) for i in range(0, len(ciphertext_bits), 8))
-    return ciphertext
+def xor_strings(s1, s2):
+    """Return XOR of two strings of equal length"""
+    return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s1, s2))
 
 
-def decrypt(ciphertext: str, key: str) -> str:
-    """
-    Decrypt ciphertext using the one-time pad scheme with a PRG key.
-    """
-    if len(key) < len(ciphertext):
-        raise ValueError("Key must be at least as long as the ciphertext")
-    # Convert ciphertext and key to binary strings
-    ciphertext_bits = ''.join(format(ord(c), '08b') for c in ciphertext)
-    key_bits = ''.join(format(int(c), '08b') for c in key)
-    # Generate the same pseudorandom bit string used to encrypt the plaintext
-    prg_bits = bin(secrets.randbits(len(ciphertext_bits)))[
-        2:].zfill(len(ciphertext_bits))
-    # XOR the ciphertext and PRG bits using Python's built-in bitwise XOR operator (^)
-    plaintext_bits = ''.join(str(int(a) ^ int(b))
-                             for a, b in zip(ciphertext_bits, prg_bits))
-    # Convert the plaintext binary string back to ASCII
-    plaintext = ''.join(
-        chr(int(plaintext_bits[i:i+8], 2)) for i in range(0, len(plaintext_bits), 8))
+def text_to_binary(text):
+    binary = ""
+    # iterate over each character in the text
+    for char in text:
+        # convert the character to its corresponding binary representation
+        binary += format(ord(char), '08b')
+    return binary
+
+
+def binary_to_text(binary):
+    # split the binary string into 8-bit chunks
+    chunks = [binary[i:i+8] for i in range(0, len(binary), 8)]
+    # convert each 8-bit chunk to its corresponding integer value
+    values = [int(chunk, 2) for chunk in chunks]
+    # convert each integer value to its corresponding ASCII character
+    text = ''.join([chr(value) for value in values])
+    return text
+
+
+def enc(key, plaintext):
+    """Encrypt plaintext using the key"""
+    ciphertext = xor_strings(key, plaintext)
+    binary = text_to_binary(ciphertext)
+    return binary
+
+
+def dec(key, binary):
+    """Decrypt binary using the key"""
+    text = binary_to_text(binary)
+    plaintext = xor_strings(key, text)
     return plaintext
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Implement a pseudo one-time pad scheme (OTP) using a pseudorandom generator (PRG)")
-    parser.add_argument(
-        "-gen", type=int, help="Generate an n-bit pseudorandom key")
-    parser.add_argument(
-        "-enc", nargs=2, help="Encrypt plaintext using the given key")
-    parser.add_argument(
-        "-dec", nargs=2, help="Decrypt ciphertext using the given key")
+        description='Pseudo one-time pad encryption/decryption')
+    parser.add_argument('-gen', type=int, help='Generate an n-bit key')
+    parser.add_argument('-enc', nargs=2, help='Encrypt plaintext using a key')
+    parser.add_argument('-dec', nargs=2, help='Decrypt ciphertext using a key')
     args = parser.parse_args()
 
     if args.gen:
-        print(gen_key(args.gen))
+        key = gen(args.gen)
+        print(key)
     elif args.enc:
         key = args.enc[0]
         plaintext = args.enc[1]
-        print(encrypt(plaintext, key))
+        ciphertext = enc(key, plaintext)
+        print(ciphertext)
     elif args.dec:
         key = args.dec[0]
         ciphertext = args.dec[1]
-        print(decrypt(ciphertext, key))
+        plaintext = dec(key, ciphertext)
+        print(plaintext)
     else:
-        print("Please provide one of the following options: -gen <n>, -enc <key> <plaintext>, -dec <key> <ciphertext>")
+        parser.print_help()
+
+
+# def test() -> str:
+#     gk = gen(11)
+#     enc1 = enc(gk, "hello there")
+#     dec1 = dec(gk, enc1)
+#     print(dec1)
+
+# test()
